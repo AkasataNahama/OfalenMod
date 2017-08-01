f3f5ba21da74481e5eb14ae96e98ad71ff159592
AkasataNahama <redsandsnake@gmail.com>
2017-08-01 14:41:43 +0900

    変更：収集機のスロットは搬入可能
    経験値の結晶のみShiftで転送する

--- src/main/java/nahama/ofalenmod/inventory/ContainerBlockCollector.java
+++ src/main/java/nahama/ofalenmod/inventory/ContainerBlockCollector.java
@@ -1,9 +1,11 @@
 package nahama.ofalenmod.inventory;
 
+import nahama.ofalenmod.item.ItemExpCrystal;
 import nahama.ofalenmod.tileentity.TileEntityCollector;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerBlockCollector extends ContainerWorldEditorBase {
 	public ContainerBlockCollector(EntityPlayer player, TileEntityCollector tileEntity) {
@@ -16,7 +18,7 @@ public class ContainerBlockCollector extends ContainerWorldEditorBase {
 	protected void addInventory(IInventory inventory) {
 		for (int iy = 0; iy < 6; iy++) {
 			for (int ix = 0; ix < 9; ix++) {
-				this.addSlotToContainer(new SlotUnputtable(inventory, ix + (iy * 9), 8 + (ix * 18), 18 + (iy * 18)));
+				this.addSlotToContainer(new Slot(inventory, ix + (iy * 9), 8 + (ix * 18), 18 + (iy * 18)));
 			}
 		}
 	}
@@ -31,5 +33,20 @@ public class ContainerBlockCollector extends ContainerWorldEditorBase {
 		for (int ix = 0; ix < 9; ix++) {
 			this.addSlotToContainer(new Slot(player.inventory, ix, 8 + (ix * 18), 198));
 		}
+	}
+
+	@Override
+	protected int getTypeStackTransfer(ItemStack itemStack) {
+		int type = super.getTypeStackTransfer(itemStack);
+		if (type == 0 && itemStack.getItem() instanceof ItemExpCrystal)
+			return 2;
+		return type;
+	}
+
+	@Override
+	protected boolean mergeStackWithType(int type, ItemStack itemStack) {
+		if (type == 1)
+			return super.mergeStackWithType(type, itemStack);
+		return type != 2 || this.mergeItemStack(itemStack, INDEX_1, INDEX_2, false);
 	}
 }
