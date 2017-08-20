d3b810c103ecc6fe390eae3b6f667ee296998c38
AkasataNahama <redsandsnake@gmail.com>
2017-08-20 18:14:06 +0900

    変更：万能ツールは詳細設定に対応する

--- src/main/java/nahama/ofalenmod/item/tool/ItemOfalenPerfectTool.java
+++ src/main/java/nahama/ofalenmod/item/tool/ItemOfalenPerfectTool.java
@@ -2,8 +2,15 @@ package nahama.ofalenmod.item.tool;
 
 import cpw.mods.fml.common.eventhandler.Event.Result;
 import nahama.ofalenmod.OfalenModCore;
+import nahama.ofalenmod.core.OfalenModBlockCore;
 import nahama.ofalenmod.core.OfalenModConfigCore;
+import nahama.ofalenmod.core.OfalenModItemCore;
 import nahama.ofalenmod.handler.OfalenKeyHandler;
+import nahama.ofalenmod.setting.IItemOfalenSettable;
+import nahama.ofalenmod.setting.OfalenSettingBoolean;
+import nahama.ofalenmod.setting.OfalenSettingCategory;
+import nahama.ofalenmod.setting.OfalenSettingCategoryOrigin;
+import nahama.ofalenmod.setting.OfalenSettingInteger;
 import nahama.ofalenmod.util.BlockRange;
 import nahama.ofalenmod.util.OfalenNBTUtil;
 import nahama.ofalenmod.util.OfalenNBTUtil.FilterUtil;
@@ -16,17 +23,22 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTool;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.UseHoeEvent;
+import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.List;
 
-public class ItemOfalenPerfectTool extends ItemTool {
+public class ItemOfalenPerfectTool extends ItemTool implements IItemOfalenSettable {
+	private OfalenSettingCategoryOrigin origin;
+
 	public ItemOfalenPerfectTool(ToolMaterial material) {
 		super(0.0F, material, null);
 		this.setCreativeTab(OfalenModCore.TAB_OFALEN);
@@ -40,9 +52,11 @@ public class ItemOfalenPerfectTool extends ItemTool {
 
 	/** 採掘速度の設定。 */
 	@Override
-	public float func_150893_a(ItemStack itemStack, Block block) {
+	public float getDigSpeed(ItemStack stack, Block block, int meta) {
 		// 他のツールでは適正ブロックの判定をするが、ここではすべてに適正採掘速度を適用する。
-		return this.efficiencyOnProperMaterial;
+		if (((OfalenSettingBoolean) this.getSetting().getChildCategory("NormalBreaking").getChildSetting("ItemFilter")).getValueByStack(stack) && !FilterUtil.canItemFilterThrough(FilterUtil.getFilterTag(stack), new ItemStack(block, 1, meta)))
+			return 1;
+		return ((OfalenSettingInteger) this.getSetting().getChildCategory("NormalBreaking").getChildSetting("BreakingEfficiency")).getValueByStack(stack);
 	}
 
 	/** アップデート時の処理。 */
@@ -85,19 +99,32 @@ public class ItemOfalenPerfectTool extends ItemTool {
 				// 次に変更できるまでの間隔を設定する。
 				itemStack.getTagCompound().setByte(OfalenNBTUtil.INTERVAL, (byte) 10);
 				if (world.isRemote) {
-					int length = this.getLength(itemStack);
-					int x = Minecraft.getMinecraft().objectMouseOver.blockX;
-					int y = Minecraft.getMinecraft().objectMouseOver.blockY;
-					int z = Minecraft.getMinecraft().objectMouseOver.blockZ;
-					BlockRange range = new BlockRange(x - length, y - length, z - length, x + length, y + length, z + length);
-					OfalenParticleUtil.spawnParticleWithBlockRange(player.worldObj, range, 2);
+					MovingObjectPosition pos = Minecraft.getMinecraft().objectMouseOver;
+					OfalenParticleUtil.spawnParticleWithBlockRange(player.worldObj, this.getRange(itemStack, pos.blockX, pos.blockY, pos.blockZ), 2);
 				}
 			}
 		} else {
 			if (world.isRemote)
 				return itemStack;
 			// 設定キーが押されていたら、modeを変更する。
-			if (!player.isSneaking()) {
+			mode = this.getNextMode(itemStack, mode, player.isSneaking());
+			itemStack.getTagCompound().setByte(OfalenNBTUtil.MODE, mode);
+			// 次に変更できるまでの間隔を設定する。
+			itemStack.getTagCompound().setByte(OfalenNBTUtil.INTERVAL, (byte) 10);
+			// プレイヤーのチャット欄に通知する。
+			if (((OfalenSettingBoolean) this.getSetting().getChildSetting("ModeChangeNotification")).getValueByStack(itemStack))
+				OfalenUtil.addChatTranslationMessage(player, "info.ofalen.toolPerfect.mode.changed", StatCollector.translateToLocal("info.ofalen.toolPerfect.mode." + mode));
+		}
+		return itemStack;
+	}
+
+	/** 次のモード番号を返す。 */
+	private byte getNextMode(ItemStack stack, int current, boolean isReverse) {
+		if (current < 0)
+			current = 0;
+		int mode = current;
+		do {
+			if (!isReverse) {
 				mode++;
 			} else {
 				mode--;
@@ -106,13 +133,10 @@ public class ItemOfalenPerfectTool extends ItemTool {
 				mode = 3;
 			if (3 < mode)
 				mode = 0;
-			itemStack.getTagCompound().setByte(OfalenNBTUtil.MODE, mode);
-			// 次に変更できるまでの間隔を設定する。
-			itemStack.getTagCompound().setByte(OfalenNBTUtil.INTERVAL, (byte) 10);
-			// プレイヤーのチャット欄に通知する。
-			OfalenUtil.addChatTranslationMessage(player, "info.ofalen.toolPerfect.mode.changed", StatCollector.translateToLocal("info.ofalen.toolPerfect.mode." + mode));
-		}
-		return itemStack;
+			if (((OfalenSettingBoolean) this.getSetting().getChildCategory("ModeChange").getChildSetting("Mode." + mode)).getValueByStack(stack))
+				return (byte) mode;
+		} while (mode != current);
+		return -1;
 	}
 
 	/** アイテムが使われた(ブロックに右クリックされた)時の処理。 */
@@ -124,15 +148,16 @@ public class ItemOfalenPerfectTool extends ItemTool {
 		switch (mode) {
 		case 0:
 			// 右クリック時効果が単一破壊の時。
-			this.breakAndDropBlockByPlayerWithFilter(itemStack, player, world, x, y, z);
+			this.breakAndDropBlockByPlayerWithFilter(itemStack, player, world, x, y, z, ((OfalenSettingBoolean) this.getSetting().getChildCategory("SingleBreaking").getChildSetting("ItemFilter")).getValueByStack(itemStack));
 			return true;
 		case 1:
 			// 右クリック時効果が範囲破壊の時。
-			int length = this.getLength(itemStack);
-			for (int ix = -length; ix <= length; ix++) {
-				for (int iy = -length; iy <= length; iy++) {
-					for (int iz = -length; iz <= length; iz++) {
-						this.breakAndDropBlockByPlayerWithFilter(itemStack, player, world, x + ix, y + iy, z + iz);
+			boolean isFilterEnabled = ((OfalenSettingBoolean) this.getSetting().getChildCategory("RangeBreaking").getChildSetting("ItemFilter")).getValueByStack(itemStack);
+			BlockRange range = this.getRange(itemStack, x, y, z);
+			for (int ix = range.posMin.x; ix <= range.posMax.x; ix++) {
+				for (int iy = range.posMax.y; iy >= range.posMin.y; iy--) {
+					for (int iz = range.posMin.z; iz <= range.posMax.z; iz++) {
+						this.breakAndDropBlockByPlayerWithFilter(itemStack, player, world, ix, iy, iz, isFilterEnabled);
 					}
 				}
 			}
@@ -172,15 +197,42 @@ public class ItemOfalenPerfectTool extends ItemTool {
 		return false;
 	}
 
+	/** 詳細設定とNBTから範囲を算出し返す。 */
+	private BlockRange getRange(ItemStack itemStack, int x, int y, int z) {
+		OfalenSettingCategory categoryDefaultRange = this.getSetting().getChildCategory("RangeBreaking").getChildCategory("DefaultRange");
+		OfalenSettingCategory categoryStrength = this.getSetting().getChildCategory("RangeBreaking").getChildCategory("Strength");
+		int addition = itemStack.getTagCompound().getByte(OfalenNBTUtil.RANGE_LENGTH_ADDITION);
+		BlockRange range = new BlockRange(x, y, z, x, y, z);
+		range.posMax.x += this.getValidLength(this.getLengthSetting(itemStack, categoryDefaultRange, "X+") + this.getLengthSetting(itemStack, categoryStrength, "X+") * addition);
+		range.posMax.y += this.getValidLength(this.getLengthSetting(itemStack, categoryDefaultRange, "Y+") + this.getLengthSetting(itemStack, categoryStrength, "Y+") * addition);
+		range.posMax.z += this.getValidLength(this.getLengthSetting(itemStack, categoryDefaultRange, "Z+") + this.getLengthSetting(itemStack, categoryStrength, "Z+") * addition);
+		range.posMin.x -= this.getValidLength(this.getLengthSetting(itemStack, categoryDefaultRange, "X-") + this.getLengthSetting(itemStack, categoryStrength, "X-") * addition);
+		range.posMin.y -= this.getValidLength(this.getLengthSetting(itemStack, categoryDefaultRange, "Y-") + this.getLengthSetting(itemStack, categoryStrength, "Y-") * addition);
+		range.posMin.z -= this.getValidLength(this.getLengthSetting(itemStack, categoryDefaultRange, "Z-") + this.getLengthSetting(itemStack, categoryStrength, "Z-") * addition);
+		return range;
+	}
+
+	/** 詳細設定から長さを取得する。 */
+	private int getLengthSetting(ItemStack itemStack, OfalenSettingCategory category, String name) {
+		return ((OfalenSettingInteger) category.getChildSetting("Length." + name)).getValueByStack(itemStack);
+	}
+
+	/** 有効な長さを返す。 */
+	private int getValidLength(int length) {
+		length = Math.max(length, 0);
+		length = Math.min(length, OfalenModConfigCore.rangeMax);
+		return length;
+	}
+
 	/** フィルターで指定されたブロックを破壊し、ドロップさせる。 */
-	private void breakAndDropBlockByPlayerWithFilter(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z) {
+	private void breakAndDropBlockByPlayerWithFilter(ItemStack itemStack, EntityPlayer player, World world, int x, int y, int z, boolean isFilterEnabled) {
 		Block block = world.getBlock(x, y, z);
 		int meta = world.getBlockMetadata(x, y, z);
 		// 破壊不可（岩盤）なら終了。
 		if (block.getBlockHardness(world, x, y, z) < 0.0F)
 			return;
 		// フィルターに引っかかったら不可。
-		if (!FilterUtil.canItemFilterThrough(FilterUtil.getFilterTag(itemStack), new ItemStack(block, 1, meta)))
+		if (isFilterEnabled && !FilterUtil.canItemFilterThrough(FilterUtil.getFilterTag(itemStack), new ItemStack(block, 1, meta)))
 			return;
 		// ブロックを壊した時、道具の耐久値を減らす処理。
 		itemStack.func_150999_a(world, block, x, y, z, player);
@@ -202,17 +254,6 @@ public class ItemOfalenPerfectTool extends ItemTool {
 			if (!EnchantmentHelper.getSilkTouchModifier(player))
 				block.dropXpOnBlockBreak(world, x, y, z, block.getExpDrop(world, meta, EnchantmentHelper.getFortuneModifier(player)));
 		}
-	}
-
-	private int getRawLength(ItemStack itemStack) {
-		return 1 + itemStack.getTagCompound().getByte(OfalenNBTUtil.RANGE_LENGTH_ADDITION);
-	}
-
-	private int getLength(ItemStack itemStack) {
-		int length = this.getRawLength(itemStack);
-		length = Math.max(length, 0);
-		length = Math.min(length, OfalenModConfigCore.rangeMax);
-		return length;
 	}
 
 	/** Entityを叩いた時の処理。 */
@@ -242,8 +283,55 @@ public class ItemOfalenPerfectTool extends ItemTool {
 		List<String> stringList = OfalenUtil.getAs(list);
 		stringList.add(StatCollector.translateToLocal("info.ofalen.toolPerfect.mode") + " : " + StatCollector.translateToLocal("info.ofalen.toolPerfect.mode." + mode));
 		if (mode == 1) {
-			stringList.add(StatCollector.translateToLocal("info.ofalen.toolPerfect.mode.1.range") + " : " + this.getLength(itemStack) + " (" + this.getRawLength(itemStack) + ")");
+			stringList.add(StatCollector.translateToLocal("info.ofalen.toolPerfect.mode.1.range") + " : " + this.getRange(itemStack, 0, 0, 0).toStringRange() + " (" + itemStack.getTagCompound().getByte(OfalenNBTUtil.RANGE_LENGTH_ADDITION) + ")");
 		}
 		stringList.addAll(FilterUtil.getFilterInformation(itemStack));
+	}
+
+	@Override
+	public OfalenSettingCategoryOrigin getSetting() {
+		if (origin != null)
+			return origin;
+		// 詳細設定の初期化を行う。
+		origin = new OfalenSettingCategoryOrigin("OfalenPerfectTool", new ItemStack(this, 1, OreDictionary.WILDCARD_VALUE));
+		// モード変更通知。
+		origin.registerChildSetting(new OfalenSettingBoolean("ModeChangeNotification", new ItemStack(Blocks.redstone_torch), true));
+		OfalenSettingCategory category = new OfalenSettingCategory("ModeChange", new ItemStack(Blocks.glass));
+		origin.registerChildSetting(category);
+		category.registerChildSetting(new OfalenSettingBoolean("Mode.0", new ItemStack(Items.gold_ingot), true));
+		category.registerChildSetting(new OfalenSettingBoolean("Mode.1", new ItemStack(Items.diamond), true));
+		category.registerChildSetting(new OfalenSettingBoolean("Mode.2", new ItemStack(Items.wheat_seeds), true));
+		category.registerChildSetting(new OfalenSettingBoolean("Mode.3", new ItemStack(Items.bone), true));
+		// 通常破壊。
+		category = new OfalenSettingCategory("NormalBreaking", new ItemStack(Items.iron_ingot));
+		origin.registerChildSetting(category);
+		category.registerChildSetting(new OfalenSettingBoolean("ItemFilter", new ItemStack(Items.string), false));
+		category.registerChildSetting(new OfalenSettingInteger("BreakingEfficiency", new ItemStack(Blocks.obsidian), 64, 0));
+		// 単一破壊。
+		category = new OfalenSettingCategory("SingleBreaking", new ItemStack(Items.gold_ingot));
+		origin.registerChildSetting(category);
+		category.registerChildSetting(new OfalenSettingBoolean("ItemFilter", new ItemStack(Items.string), false));
+		// 範囲破壊。
+		category = new OfalenSettingCategory("RangeBreaking", new ItemStack(Items.diamond));
+		origin.registerChildSetting(category);
+		category.registerChildSetting(new OfalenSettingBoolean("ItemFilter", new ItemStack(Items.string), true));
+		category.registerChildSetting(new OfalenSettingBoolean("EyeResponse", new ItemStack(Items.stick), true));
+		OfalenSettingCategory category1 = new OfalenSettingCategory("DefaultRange", new ItemStack(OfalenModBlockCore.surveyorOfalen));
+		category.registerChildSetting(category1);
+		category1.registerChildSetting(new OfalenSettingInteger("Length.X+", new ItemStack(OfalenModItemCore.gemOfalen, 1, 0), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Y+", new ItemStack(OfalenModItemCore.gemOfalen, 1, 1), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Z+", new ItemStack(OfalenModItemCore.gemOfalen, 1, 2), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.X-", new ItemStack(OfalenModItemCore.gemOfalen, 1, 4), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Y-", new ItemStack(OfalenModItemCore.gemOfalen, 1, 5), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Z-", new ItemStack(OfalenModItemCore.gemOfalen, 1, 6), 1, 0));
+		category1 = new OfalenSettingCategory("Strength", new ItemStack(OfalenModItemCore.wandSurveying));
+		category.registerChildSetting(category1);
+		category1.registerChildSetting(new OfalenSettingInteger("Length.X+", new ItemStack(OfalenModItemCore.gemOfalen, 1, 0), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Y+", new ItemStack(OfalenModItemCore.gemOfalen, 1, 1), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Z+", new ItemStack(OfalenModItemCore.gemOfalen, 1, 2), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.X-", new ItemStack(OfalenModItemCore.gemOfalen, 1, 4), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Y-", new ItemStack(OfalenModItemCore.gemOfalen, 1, 5), 1, 0));
+		category1.registerChildSetting(new OfalenSettingInteger("Length.Z-", new ItemStack(OfalenModItemCore.gemOfalen, 1, 6), 1, 0));
+		return origin;
 	}
 }
